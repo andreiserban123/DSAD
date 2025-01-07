e8578ed67e20e0b6a92d0299addb7d347cc9c7a3
andreiserban123 <andyspeed2003@gmail.com>
2025-01-07 16:21:42 +0200

    remove

--- exTest/ex3/3.py
+++
@@ -1,8 +0,0 @@
-# ex: creati o lista de nume de studenti si note. filtrati studentii cu note mai mari de 8
-
-students = [("John", 7.2), ("Alice", 9.5), ("Bob", 8), ("Diana", 10), ("George", 6)]
-
-def filterStudents(students):
-    return [student for student in students if student[1] > 8]
-
-print(filterStudents(students))
